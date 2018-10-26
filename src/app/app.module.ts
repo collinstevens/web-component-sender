245649f94815d8b9ef4b5b07dfd7c53f301d480f
Collin Stevens <collin.michael.stevens@gmail.com>
2018-10-26 06:15:40 -0700

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReceiverComponent } from './receiver/receiver.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ReceiverComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent],
+  entryComponents: [ReceiverComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const senderElement = createCustomElement(ReceiverComponent, {
+      injector: this.injector
+    });
+    customElements.define('web-receiver', senderElement);
+  }
+}
---
+++ src/app/receiver/receiver.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-receiver',
+  templateUrl: './receiver.component.html',
+  styleUrls: ['./receiver.component.scss']
+})
+export class ReceiverComponent {
+  @Input()
+  myValue: string | undefined = undefined;
+}
